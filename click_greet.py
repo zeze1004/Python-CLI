7125213958e1b1d0494893bc3707a4e17ca033b5
mac <mac@daangn.com>
2022-01-06 22:13:22 +0900

    문자열 출력 코드 수정

--- click_greet.py
+++ click_greet.py
@@ -1,28 +1,21 @@
 import click
-import sys
-
 
 def say_hello(name):
-    # print와 같지만, python2역시 지원하기 위해 사용하는 click.echo
-    click.echo("Hello, {name}")
-
+    click.echo("Hello, %s" %name)ß
 
 def say_goodbye(name):
-    click.echo("Good bye, {name}")
+    click.echo("Good ßbye, %s" %name)
 
-
-# click 데코레이터
 @click.command()
 @click.option('--bye', is_flag=True, help="say good bye.")
 @click.option('--hello', is_flag=True, help="say hello.")
-def main(name, hello, bye):
+@click.optionß("--name", prompt="Your name", help="The person to greet.")
 
-    # 함수명과 인자명을 구분하기 위해 say_hello / say_goodbye로 함수명을 바꿈
+def main(name, hello, ßbye):
     if bye:
         say_goodbye(name)
     else:
         say_hello(name)
-
 
 if __name__ == "__main__":
     main()

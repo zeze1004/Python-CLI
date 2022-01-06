9e5b0f1cef6d360f8bc8e640b4d6aef19f3896e5
mac <mac@daangn.com>
2022-01-06 20:30:36 +0900

    코드 수정

--- click_greet.py
+++ click_greet.py
@@ -4,24 +4,18 @@ import sys
 
 def say_hello(name):
     # print와 같지만, python2역시 지원하기 위해 사용하는 click.echo
-    click.echo("Hello, {}".format(name))
+    click.echo("Hello, {name}")
 
 
 def say_goodbye(name):
-    click.echo('Good bye, {}'.format(name))
+    click.echo("Good bye, {name}")
 
 
 # click 데코레이터
+@click.command()
 @click.option('--bye', is_flag=True, help="say good bye.")
 @click.option('--hello', is_flag=True, help="say hello.")
-@click.option('-v', '--version', is_flag=True, help="Show version of this program.")
-@click.argument('name')
-@click.command()
-def main(name, version, hello, bye):
-
-    if version:
-        print('1.0.0')
-        sys.exit()
+def main(name, hello, bye):
 
     # 함수명과 인자명을 구분하기 위해 say_hello / say_goodbye로 함수명을 바꿈
     if bye:

01bc5159495c98cabf669fc3f0936c7aa964d0b4
mac <mac@daangn.com>
2022-01-07 01:13:51 +0900

    end 입력시 프로그램 종료 구현

---
+++ prototype.py
@@ -0,0 +1,18 @@
+import click
+
+@click.command()
+@click.option("--input", prompt="Your input")
+
+
+def communication(input):
+    while input != "end":
+        communication2(input)
+    exit()
+
+def communication2(input):
+    click.echo("%s" %input)
+    communication()
+
+if __name__ == '__main__':
+    print("프로그램 종료를 원하시면 end를 입력해주세요")
+    communication()

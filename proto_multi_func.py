6b4e2e28b14e2ff77bad6d2cca65bc26a0109a33
mac <mac@daangn.com>
2022-01-07 17:23:33 +0900

    선택지 추가

---
+++ proto_multi_func.py
@@ -0,0 +1,32 @@
+import click
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+def create():
+    click.echo('Initialized the redis')
+
+@cli.command()
+def delete():
+    click.echo('Deleted the redis')
+
+def communication(input_str):
+    while input_str != "end":
+        if input_str == "create":
+            create()
+        elif input_str == "delete":
+            delete()
+        else:
+            print("다시 입력하시오")
+            input_str = input("명령어를 입력하시오: ")
+    exit()
+
+if __name__ == '__main__':
+    print("프로그램 종료를 원하시면 end를 입력해주세요")
+    print("redis 생성: create / 삭제 delete")
+    input_str = input("명령어를 입력하세요: ")
+    communication(input_str)
+
+    
--- proto_one_func.py
+++ proto_one_func.py
@@ -3,7 +3,6 @@ import click
 @click.command()
 @click.option("--input", prompt="Your input")
 
-
 def communication(input):
     while input != "end":
         communication2(input)
@@ -13,6 +12,8 @@ def communication2(input):
     click.echo("%s" %input)
     communication()
 
+
 if __name__ == '__main__':
     print("프로그램 종료를 원하시면 end를 입력해주세요")
     communication()
+    
